bc209a7a4bdc0d00aa5930e10dd8862943dd440c
Eunsu Park <phd.choux@gmail.com>
2025-01-03 12:00:06 +0900

    update

--- drowsypy/transfer/http.py
+++ drowsypy/transfer/http.py
@@ -2,6 +2,10 @@ import os
 import tempfile
 import shutil
 import requests
+from urllib.parse import urljoin
+import urllib.request as urllib
+import ssl
+from bs4 import BeautifulSoup
 
 
 class HTTPClient:
@@ -37,8 +41,40 @@ class HTTPClient:
             message = f"Error during requests to {url} : {str(e)}"
             print(message)
             return False
+        
+    def search(self, base_url, ext):
+        if base_url[-1] != '/':
+            base_url += '/'
+        response = requests.get(base_url, verify=False)
+        if response.status_code == 200:
+            urls = []
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for link in soup.find_all('a'):
+                href = link.get('href')
+                if href and (href.endswith(f".{ext.lower()}") or href.endswith(f".{ext.upper()}")):
+                    urls.append(urljoin(base_url, href))
+            print(f"Found {len(urls)} data files {base_url}")
+            return urls
+        elif response.status_code == 404:
+            print(f"Error: {base_url} not found")
+            return []
+        else :
+            print(f"Error: {base_url} status code {response.status_code}")
+            return []
 
-    def download(self, source:str=None, destination:str=None, overwrite:bool=False) -> bool:
+
+    def download_url(self, source:str=None, destination:str=None, overwrite:bool=False) -> bool:
+        context = ssl._create_unverified_context()
+        fp = urllib.urlopen(source, timeout=30, context=context)
+        content = fp.read()
+        destination = open(destination, "wb")
+        destination.write(content)
+        fp.close()
+        destination.close()
+
+
+    def download_file(self, source:str=None, destination:str=None, overwrite:bool=False) -> bool:
         """
         Download file from HTTP.
 
